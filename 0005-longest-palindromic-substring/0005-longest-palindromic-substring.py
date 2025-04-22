6e1802c6773bbb6dc7317c9b3144c17dcfa8fe7f
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-04-22 16:05:24 -0700

    Time: 271 ms (69.13%), Space: 18.1 MB (29.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        n = len(s)
         def extend(s, i, j):
-            while i > 0 and j < len(s)-1 and s[i-1] == s[j+1]:
+            n = len(s)
+            while i > 0 and j < n-1 and s[i-1] == s[j+1]:
                 i -= 1
                 j += 1
             return (i, j)
         max_len = 0
-        longest_pal = ""
-        for i in range(len(s)):
-            max_i, max_j = extend(s, i, i)
-            if max_j - max_i + 1 > max_len:
-                max_len = max_j - max_i + 1
-                longest_pal = s[max_i : max_j + 1]
-        for i in range(1, len(s)):
-            max_i, max_j = extend(s, i, i-1)
-            if max_j - max_i + 1 > max_len:
-                max_len = max_j - max_i + 1
-                longest_pal = s[max_i : max_j + 1]
-        return longest_pal
-
-
-        
+        max_pal = ""
+        for i in range(n):
+            maxi, maxj = extend(s, i, i) 
+            curr_len = maxj - maxi + 1
+            if curr_len > max_len:
+                max_len = curr_len
+                max_pal = s[maxi:maxj+1]
+        for i in range(1, n):
+            if s[i-1] == s[i]:
+                maxi, maxj = extend(s, i-1, i) 
+                curr_len = maxj - maxi + 1
+                if curr_len > max_len:
+                    max_len = curr_len
+                    max_pal = s[maxi:maxj+1]
+        return max_pal
