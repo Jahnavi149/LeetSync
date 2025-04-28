5ff3384826b66d29a93e7ad08ca4462b6e15177e
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-04-28 09:16:43 -0700

    Time: 387 ms (67.42%), Space: 17.9 MB (78.34%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.py
+++ 1511-count-number-of-teams/1511-count-number-of-teams.py
@@ -2,24 +2,18 @@ class Solution:
     def numTeams(self, rating: List[int]) -> int:
         n = len(rating)
         result = 0
-        
         for j in range(n):
-            leftSmaller = leftGreater = 0
-            rightSmaller = rightGreater = 0
-            
+            left_smaller = left_larger = right_smaller = right_larger = 0
             for i in range(j):
                 if rating[i] < rating[j]:
-                    leftSmaller += 1
-                elif rating[i] > rating[j]:
-                    leftGreater += 1
-            
-            for k in range(j + 1, n):
+                    left_smaller += 1
+                else:
+                    left_larger += 1
+            for k in range(j+1, n):
                 if rating[k] > rating[j]:
-                    rightGreater += 1
-                elif rating[k] < rating[j]:
-                    rightSmaller += 1
-            
-            result += leftSmaller * rightGreater + leftGreater * rightSmaller
-            
+                    right_larger += 1
+                else:
+                    right_smaller += 1
+            result += (left_smaller*right_larger) + (left_larger * right_smaller)
+
         return result
-        
