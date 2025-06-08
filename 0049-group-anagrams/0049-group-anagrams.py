cad6d7b5640941aceadf946dd5aa75221e20a0ca
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-07 20:19:43 -0700

    Time: 12 ms (60.58%), Space: 22.4 MB (35.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -5,12 +5,10 @@ class Solution:
             counter = [0]*26
             for ch in st:
                 counter[ord(ch) - ord('a')] += 1
-            counter_str = str(counter)
+            counter_str = tuple(counter)
             if counter_str in anagrams_dict:
                 anagrams_dict[counter_str].append(st)
             else:
                 anagrams_dict[counter_str] = [st]
-        print(anagrams_dict)
-        print(anagrams_dict.values())
         return list(anagrams_dict.values())
         
