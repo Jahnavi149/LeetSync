37c8bf0b93db661e57471e556eb1509301f557ee
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-07 20:32:29 -0700

    Time: 7 ms (72.73%), Space: 20.8 MB (89.5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,9 +4,10 @@ class Solution:
         intervals.sort(key = lambda x: x[0])
         merged.append(intervals[0])
         for i in range(1, len(intervals)):
-            if merged[-1][1] >= intervals[i][0]:
-                merged[-1][1] = max(merged[-1][1], intervals[i][1])
-            else:
+            if merged[-1][1] < intervals[i][0]:
                 merged.append(intervals[i])
+            else:
+                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+                merged[-1][0] = min(merged[-1][0], intervals[i][0])
         return merged
         
