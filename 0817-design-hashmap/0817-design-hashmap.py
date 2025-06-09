c5394f430a35103798193f331037fd4ea342092e
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-08 21:56:40 -0700

    Time: 30 ms (88.48%), Space: 21 MB (72.55%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,17 +1,55 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+
 class MyHashMap:
 
     def __init__(self):
-        self.cap = pow(10, 6)+1
-        self.hash = [-1]*self.cap
-
+        self.hashmap = [None]*1000
+        
     def put(self, key: int, value: int) -> None:
-        self.hash[key] = value
+        hash_key = key%1000
+        if self.hashmap[hash_key] is None:
+            self.hashmap[hash_key] = Node(key, value)
+            return
+        else:
+            node = self.hashmap[hash_key]
+            while node:
+                if node.key == key:
+                    node.val = value
+                    return
+                if node.next is None:
+                    break
+                node = node.next
+            node.next = Node(key, value)
+
         
     def get(self, key: int) -> int:
-        return self.hash[key]
+        hash_key = key%1000
+        node = self.hashmap[hash_key]
+        while node:
+            if node.key == key:
+                return node.val
+            node = node.next
+        return -1
         
     def remove(self, key: int) -> None:
-        self.hash[key] = -1
+        hash_key = key%1000
+        node = self.hashmap[hash_key]
+        if node is None:
+            return
+        if node.key == key:
+            self.hashmap[hash_key] = node.next
+            return
+        while node.next:
+            if node.next.key == key:
+                node.next = node.next.next
+                return
+            node = node.next
+
+
         
 
 
