546a16d570a4ef12dbfb2ca9f3986767d9f54a94
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-08 23:34:04 -0700

    Time: 10 ms (24.59%), Space: 19.1 MB (9.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,6 @@ class Solution:
         left = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                if i != left:
-                    nums[i], nums[left] = nums[left], nums[i]
+                nums[i], nums[left] = nums[left], nums[i]
                 left += 1
         return nums
