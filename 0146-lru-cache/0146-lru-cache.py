60a2b8e9de1515f0a42a3963f6fb58316bc9e8ea
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-09 16:26:18 -0700

    Time: 140 ms (56.51%), Space: 78.9 MB (8.34%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -10,21 +10,21 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-
         self.oldest = Node(0, 0)
         self.latest = Node(0, 0)
+
         self.oldest.next = self.latest
         self.latest.prev = self.oldest
-    
-    # Inserting at the end 
+
+    # Insert as the latest 
     def _insert(self, node):
         prev = self.latest.prev
         prev.next = node
-        node.next = self.latest
         node.prev = prev
+        node.next = self.latest
         self.latest.prev = node
 
-    # Remove node from dll
+    # Remove given node
     def _remove(self, node):
         prev, next = node.prev, node.next
         prev.next = next
@@ -37,24 +37,24 @@ class LRUCache:
             self._insert(node)
             return node.val
         return -1
-
+        
     def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            if len(self.cache) == self.capacity:
-                del self.cache[self.oldest.next.key]
-                self._remove(self.oldest.next)
-        else:
-            self._remove(self.cache[key])
-        node = Node(key, value)
-        self.cache[key] = node
-        self._insert(node)
-"""        
-cap = 2
-put(1,1) : 1 -> Node(1), Node(0) -> Node(1) -> Node(0)
-put(2,2) : 2 -> Node(2), Node(0) -> Node(1) -> Node(2) -> Node(0)
-get(1) : Node(0) -> Node(2) -> Node(1) -> Node(0)
-put(3,3) : 3 -> Node(3), Node(0) -> Node(1) -> Node(0), Node(0) -> Node(1) -> Node(3) -> Node(0)
-"""
+        if key in self.cache:
+            node = self.cache[key]
+            del self.cache[key]
+            self._remove(node)
+
+        if len(self.cache) == self.capacity:
+            oldest = self.oldest.next
+            del self.cache[oldest.key]
+            self._remove(oldest)
+
+        new_node = Node(key, value)
+        self._insert(new_node)
+        self.cache[key] = new_node
+
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
