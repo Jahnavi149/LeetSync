a4340f3b2d7fd4a2fad6dbff3d94aa956b18ed3b
Jahnavi Lavu <73525929+Jahnavi149@users.noreply.github.com>
2025-06-16 13:40:00 -0700

    Time: 14 ms (99.62%), Space: 47.7 MB (90.15%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -8,56 +8,46 @@ class Node {
         this.val = val;
         this.next = null;
     }
-
-    public int getKey(){
-        return key;
-    }
-    public int getVal(){
-        return val;
-    }
-    public void setVal(int value){
-        val = value;
-    }
 }
 
-
 class MyHashMap {
-    private Node[] arr;
+    Node[] arr;
 
     public MyHashMap() {
         this.arr = new Node[1000];
+        
     }
     
     public void put(int key, int value) {
-        int hashKey = key%1000;
-        Node node = arr[hashKey]; 
-        if(node == null){ arr[hashKey] = new Node(key, value); return; }
+        int hashCode = key%1000;
+        Node node = arr[hashCode];
+        if(node == null){ arr[hashCode] = new Node(key, value); return; }
         while(node != null){
-            if(node.getKey() == key){ node.setVal(value); return; }
-            if(node.next == null){ node.next = new Node(key, value); return; }
+            if(node.key == key){ node.val = value; return; }
+            if(node.next == null){ node.next = new Node(key, value); }
             node = node.next;
-        }     
+        }    
     }
     
     public int get(int key) {
-        int hashKey = key%1000;
-        Node node = arr[hashKey];
+        int hashCode = key%1000;
+        Node node = arr[hashCode];
         while(node != null){
-            if (node.getKey() == key){ return node.getVal(); }
+            if(node.key == key){ return node.val; }
             node = node.next;
         }
-        return -1;        
+        return -1;   
     }
     
     public void remove(int key) {
-        int hashKey = key%1000;
-        Node node = arr[hashKey];
-        if(node == null){return;} 
-        if (node.getKey() == key){ arr[hashKey] = node.next; return;}
+        int hashCode = key%1000;
+        Node node = arr[hashCode];
+        if(node == null){ return; }
+        if(node.key == key){ arr[hashCode] = node.next; return; }
         while(node.next != null){
-            if (node.next.getKey() == key) { node.next = node.next.next; return; }
+            if(node.next.key == key){ node.next = node.next.next; return;}
             node = node.next;
-        }      
+        }   
     }
 }
 
